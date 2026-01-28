21c7c70c22f5679275a1ff79b152fa79cb8cf006
duongcoder211 <daomanhduong0211@gmail.com>
2026-01-29 00:02:23 +0700

    first commit

--- utils/readcsv.py
+++ utils/readcsv.py
@@ -5,5 +5,5 @@ def read_csv(path) -> list[dict[str,str]]:
     with open(path, mode='r', newline='', encoding="utf-8") as file:
         reader = csv.reader(file, delimiter=";")
         for row in list(reader)[1:]:
-            data.append({"slang": row[0], "mean": row[1]})
+            data.append({"slang": row[0], "mean": row[1].replace('\\\"', '"')})
     return data
